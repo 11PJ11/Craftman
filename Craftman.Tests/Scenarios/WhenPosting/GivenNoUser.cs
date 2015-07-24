d03eb30135f2f4a170c23524e73a7a620b149a7b
Parajao <alex.digioia@gmail.com>
2015-07-24 12:31:49 +0200

    moved sceanrio file

---
+++ Craftman.Tests/Scenarios/WhenPosting/GivenNoUser.cs
@@ -0,0 +1,32 @@
+﻿using Craftman.Tests.Scenarios.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Craftman.Tests.Scenarios.WhenPosting
+{
+    [TestFixture]
+    public class GivenNoUser
+    {
+        private TheApplication _theApp;
+
+        [SetUp]
+        public void Setup()
+        {
+            _theApp = new TheApplication();
+        }
+         
+        [Test]
+        public void ItShouldCreateTheUser()
+        {
+            //g
+
+            //w
+            _theApp.UserPosts("Alice","I Love the weather today");
+            
+            //t
+            _theApp.TimelineFor("Alice");
+            var output = _theApp.OutPut;
+            output.Should().Contain("I Love the weather today");
+        }
+    }
+}
