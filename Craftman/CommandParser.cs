9244e9cf5d7552de716f1e8c4d6affc857f48015
Parajao <alex.digioia@gmail.com>
2015-07-25 15:23:27 +0200

    refactored command creation

--- Craftman/CommandParser.cs
+++ Craftman/CommandParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Craftman.Commands;
 
@@ -10,26 +11,63 @@ namespace Craftman
         private const string QuitPattern = @"[Q|q]uit";
         private const string PostMessagePattern = @"(?<userName>\w+) -> (?<message>.*)";
 
-        public ICommand Parse(string command)
+        public ICommand Parse(string input)
         {
-            var quitReg = new Regex(QuitPattern);
-            var quitMatch = quitReg.Match(command);
-            if (quitMatch.Success)
-            {
-                return new QuitCommand();
-            }
+            var matchToCommand = _patternToCommand
+                .Select(kvp =>
+                {
+                    var pattern = kvp.Key;
+                    var commandFactory = kvp.Value;
+                    var regex = new Regex(pattern);
+                    var match = regex.Match(input);
+                    return new KeyValuePair<Match, Func<Match, ICommand>>(
+                        match,commandFactory);
+                });
 
-            var postMessageReg = new Regex(PostMessagePattern);
-            var postMessageMatch = postMessageReg.Match(command);
-            if (postMessageMatch.Success)
-            {
-                var userName = postMessageMatch.Groups["userName"].Value;
-                var message = postMessageMatch.Groups["message"].Value;
-                return new PostCommand(userName, message);
-            }
-            return new VoidCommand();
+            var successfulMatchesToCommand = matchToCommand
+                .Where(kvp =>
+                {
+                    var match = kvp.Key;
+                    return match.Success;
+                })
+                .DefaultIfEmpty(new KeyValuePair<Match, Func<Match, ICommand>>
+                    (null, match => new VoidCommand()));
+
+            var command = successfulMatchesToCommand
+                            .Select(kvp =>
+                            {
+                                var match = kvp.Key;
+                                var factory = kvp.Value;
+                                return factory(match);
+                            })
+                            .Single();
+
+            return command;
         }
 
+        private readonly Dictionary<string, Func<Match, ICommand>> 
+            _patternToCommand =
+            new Dictionary<string, Func<Match, ICommand>>
+			{
+                {QuitPattern,CommandFactory.CreateQuitCommand},
+				{PostMessagePattern, CommandFactory.CreatePostCommand}
+			};
         
+    }
+
+    public static class CommandFactory
+    {
+        public static QuitCommand CreateQuitCommand(Match quitMatch)
+        {
+            return new QuitCommand();
+        }
+
+        public static PostCommand CreatePostCommand(Match postMessageMatch)
+        {
+            var userName = postMessageMatch.Groups["userName"].Value;
+            var message = postMessageMatch.Groups["message"].Value;
+            return new PostCommand(userName, message);
+        }
+
     }
 }
