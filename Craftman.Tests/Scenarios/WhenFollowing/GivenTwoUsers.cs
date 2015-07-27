9d91831be3e579b0bac220f2ffc73d56429b20ab
Parajao <alex.digioia@gmail.com>
2015-07-27 05:49:44 +0200

    second follow scenario

---
+++ Craftman.Tests/Scenarios/WhenFollowing/GivenTwoUsers.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Craftman.Tests.Scenarios.Helpers;
+using NUnit.Framework;
+
+namespace Craftman.Tests.Scenarios.WhenFollowing
+{
+    [TestFixture]
+    public class GivenTwoUsers
+    {
+        private TheApplication _theApp;
+
+        [SetUp]
+        public void Setup()
+        {
+            _theApp = new TheApplication();
+        }
+
+        [Test]
+        public void ItShouldShowTheAggregatedTimelines()
+        {
+            //g
+            _theApp.UserPosts("Alice", "I love the weather today");
+            _theApp.UserPosts("Charlie", "I’m in New York today! Anyone want to have a coffee?");
+            var expected = new List<string>
+            {
+                "I’m in New York today! Anyone want to have a coffee? (1 second ago)",
+                "I love the weather today (1 second ago)"
+            };
+            //w
+            _theApp.UserFollowsUser("Charlie", "Alice");
+
+            //t
+            _theApp.UserWall("Charlie");
+            _theApp.Quit();
+            var output = _theApp.CollectOutPut();
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
